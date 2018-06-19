168eb213fac7b7e28558f6930d5e3db8fe0a2337
Schubi <Schubi@github.com>
2018-06-19 08:42:04 +0200

    Update:>

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - Maven muss nich nach jeder veränderung des Codes neugestartet werden!
 - Die Bower Components müssen im Static Ordner untergebracht werden.
 - Einfache Skriptbefehle funktionieren in der HTML Datei
+
+<img src="Doku/Java_Print.png">
+
+- "appFelix.run(args);" ruf Spring auf
--- src/main/java/com/explorer/SpringBootAndPolymerApplication.java
+++ src/main/java/com/explorer/SpringBootAndPolymerApplication.java
@@ -6,7 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAndPolymerApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAndPolymerApplication.class, args);
+	
+	SpringApplication appFelix = new SpringApplication(SpringBootAndPolymerApplication.class);
+
+	System.out.println("Das ist ein Test!");
+	
+   	appFelix.run(args);
+
 	}
+
 }
+
+
